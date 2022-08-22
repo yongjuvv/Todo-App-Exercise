359278e05f3e1aa678451848b7975ddfeec9024e
o01071767279@nate.com <o01071767279@nate.com>
2022-08-22 10:00:22 +0900

    Add controller

--- src/main/java/com/example/todo/controller/Controller.java
+++ src/main/java/com/example/todo/controller/Controller.java
@@ -1,2 +1,13 @@
-package com.example.todo.controller;public class Controller {
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "To-do Application !";
+    }
 }
